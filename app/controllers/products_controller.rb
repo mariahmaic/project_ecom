6c64eb428a9beb3da383f89dbc7169d9f0836f17
Mariah <mariahmaic23@gmail.com>
2022-11-24 11:26:51 -0600

    Added image functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < InheritedResources::Base
   private
 
     def product_params
-      params.require(:product).permit(:name, :price, :description, :category_id)
+      params.require(:product).permit(:name, :price, :description, :category_id, :image_filename)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_144825) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_165700) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_144825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.string "image_filename"
   end
 
   create_table "users", force: :cascade do |t|
