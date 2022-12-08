fd849992381e8a999d5c8de3f51efeca45c4f36d
Mariah <mariahmaic23@gmail.com>
2022-12-07 21:58:43 -0600

    Add items to cart with session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
+
   resources :categories
   root :to => 'store#index', :via => :get
   get 'store/:id' => 'store#show', :as => :store_product, :via => :get
 
   get 'search' => 'store#search', :as => 'search', :via => :get
   get 'search_results' => 'store#search_results', :as => 'search_results', :via => :post
+  get 'about' => 'products#about'
 
   resources :customers
   resources :products
